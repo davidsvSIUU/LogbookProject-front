2c5f0194d4867a6d6ec0b04b35e34aa5d6f30585
ASSEF Andrew <assef.andrew@gmail.com>
2025-02-17 13:05:56 +0100

    ajout du .env

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://myjauxextjrucjpkskky.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im15amF1eGV4dGpydWNqcGtza2t5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczOTc4NjY1NiwiZXhwIjoyMDU1MzYyNjU2fQ.YjWr3Zp3spPFo4PbRvzggo0zm0MNhXyB_NJepOuhhYY'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
