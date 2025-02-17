663db04de4a0d4a327f8ab5fe7a0e2b29bebabf8
davidsvSIUU <david.sv04@outlook.fr>
2025-02-17 11:49:57 +0100

    base clean

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './components/Login'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<div>Dashboard</div>} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Login from './Login'
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />,
-  },
-])
+import App from './App'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
